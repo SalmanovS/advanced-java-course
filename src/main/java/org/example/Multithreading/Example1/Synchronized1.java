949178737fd58380b192b0e9a465ff317ec2ef90
Said <narprime@yandex.ru>
2023-04-09 16:51:32 +0300

    synchronized method and synchronized blocks

---
+++ src/main/java/org/example/Multithreading/Example1/Synchronized1.java
@@ -0,0 +1,33 @@
+package org.example.Multithreading.Example1;
+
+public class Synchronized1 {
+    static Object lock = new Object();
+    static int customer = 0;
+   void queue(){
+       synchronized (lock) {
+           customer++;
+           System.out.println("Customer number " + customer + ", please come to window number 1");
+           try {
+               Thread.sleep(2000);
+           } catch (InterruptedException e) {
+               throw new RuntimeException(e);
+           }
+       }
+
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++){
+            new Thread(new MyRunnable()).start();
+        }
+    }
+}
+
+
+class MyRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        new Synchronized1().queue();
+    }
+}
